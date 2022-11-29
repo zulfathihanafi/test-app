037e7325d94ec4fa4986eb7610d107545ff51979
zulfathihanafi <fathiimran7@gmail.com>
2022-11-29 21:43:58 +0800

    To deplot

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+
+
 import './App.css';
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
+
+import Home from './Home';
+import Error from './Error';
+import About from './about';
+
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    <Router> {/** Parent component for all React Router component such as Route and Link */}
+      
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+        </ul>
+      </div>
+
+      <Routes> {/** Wrapper for all route */}
+        <Route exact path='/' element={<Home />}></Route>
+        <Route exact path='/about' element={<About />}></Route>
+
+        {/* Catch any other path other than specified path */}
+        <Route exact path='/*' element={<Error />}></Route>
+      </Routes>
+    </Router>
+
+  )
 }
 
 export default App;
---
+++ src/Error.js
@@ -0,0 +1,7 @@
+import React from 'react';
+  
+function Error (){
+    return <h1>Error 404 : Wrong Path</h1>
+}
+  
+export default Error;
